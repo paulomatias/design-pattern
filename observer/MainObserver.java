08f69df0a7f43dba3b838961f6c666645095cef0
Matias Paulo <matias0paulo@gmail.com>
2017-06-24 13:32:15 +0200

    Observer Pattern - Comments

--- observer/MainObserver.java
+++ observer/MainObserver.java
@@ -6,6 +6,10 @@ import observer.display.StatisticDisplay;
 
 public class MainObserver {
     public static void main() {
+        // Design Principles used : use loosely coupled designs
+        // Obersever Pattern : defines a one-to-many dependency between objects so that when one object changes state,
+        // all its dependents are notified and updated automatically
+        
         WeatherData weatherData = new WeatherData();
         // slightly better initialization, but we can do better
         StatisticDisplay statisticDisplay = new StatisticDisplay(weatherData);
